ad24326ede795eeb929af1cf3ae70cc4b6caf4e2
kristoffer petersen <krizzmp@gmail.com>
2018-08-24 16:40:35 +0200

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,12 @@
 import * as React from "react";
-import "./App.css";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import firebase from "firebase";
-import { app } from "./firebase-init";
-var provider = new firebase.auth.GoogleAuthProvider();
-interface RenderProps<T> {
-  children: ((props: T) => React.ReactNode);
-}
-type $Auth = RenderProps<{
-  loggedIn: boolean;
-  user?: firebase.User;
-  signIn: () => void;
-  signOut: () => void;
-}>;
-
-class Auth extends React.Component<
-  $Auth,
-  { user: firebase.User; loggedIn: boolean }
-> {
-  state = {
-    user: null,
-    loggedIn: false,
-  };
-  unsub: firebase.Unsubscribe;
-  componentDidMount() {
-    this.unsub = firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        this.setState({ user, loggedIn: true });
-      } else {
-        this.setState({ user: null, loggedIn: false });
-      }
-    });
-  }
-  componentWillUnmount() {
-    this.unsub();
-  }
-
-  signIn() {
-    app.auth().signInWithPopup(provider);
-  }
-  signOut() {
-    app.auth().signOut();
-  }
-  render() {
-    return this.props.children({
-      loggedIn: this.state.loggedIn,
-      user: this.state.user,
-      signIn: this.signIn,
-      signOut: this.signOut,
-    });
-  }
-}
-
-const Home = ({
-  user,
-  signOut,
-}: {
+import { Auth } from "./Auth";
+interface $Home {
   user: firebase.User;
   signOut: () => void;
-}) => (
+}
+const Home = ({ user, signOut }: $Home) => (
   <div>
     hello {user.displayName} <button onClick={signOut}>sign out</button>
   </div>
@@ -70,8 +18,8 @@ const SignIn = ({ signIn }: { signIn: () => void }) => (
     <button onClick={signIn}>sign in with Google</button>
   </div>
 );
-const Dash = () => (
-  <div>
+const App = () => (
+  <BrowserRouter>
     <Auth>
       {({ loggedIn, user, signIn, signOut }) =>
         loggedIn ? (
@@ -81,17 +29,7 @@ const Dash = () => (
         )
       }
     </Auth>
-  </div>
+  </BrowserRouter>
 );
-class App extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <Route path="/" component={Dash} />
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+
 export default App;
---
+++ src/Auth.tsx
@@ -0,0 +1,52 @@
+import firebase from "firebase";
+import { app } from "./firebase-init";
+import React from "react";
+var provider = new firebase.auth.GoogleAuthProvider();
+interface RenderProps<T> {
+  children: ((props: T) => React.ReactNode);
+}
+type $Auth = RenderProps<{
+  loggedIn: boolean;
+  user?: firebase.User;
+  signIn: () => void;
+  signOut: () => void;
+}>;
+export class Auth extends React.Component<
+  $Auth,
+  {
+    user: firebase.User;
+    loggedIn: boolean;
+  }
+> {
+  state = {
+    user: null,
+    loggedIn: false,
+  };
+  unsub: firebase.Unsubscribe;
+  componentDidMount() {
+    this.unsub = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user, loggedIn: true });
+      } else {
+        this.setState({ user: null, loggedIn: false });
+      }
+    });
+  }
+  componentWillUnmount() {
+    this.unsub();
+  }
+  signIn() {
+    app.auth().signInWithPopup(provider);
+  }
+  signOut() {
+    app.auth().signOut();
+  }
+  render() {
+    return this.props.children({
+      loggedIn: this.state.loggedIn,
+      user: this.state.user,
+      signIn: this.signIn,
+      signOut: this.signOut,
+    });
+  }
+}
