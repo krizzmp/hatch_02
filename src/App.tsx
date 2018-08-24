4f59b629ff0613b66ffa8778c808dfde46a85d3f
kristoffer petersen <krizzmp@gmail.com>
2018-08-24 16:07:43 +0200

    add signout button

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,13 @@ var provider = new firebase.auth.GoogleAuthProvider();
 interface RenderProps<T> {
   children: ((props: T) => React.ReactNode);
 }
-type $Auth = RenderProps<{ loggedIn: boolean; user?: firebase.User }>;
+type $Auth = RenderProps<{
+  loggedIn: boolean;
+  user?: firebase.User;
+  signIn: () => void;
+  signOut: () => void;
+}>;
+
 class Auth extends React.Component<
   $Auth,
   { user: firebase.User; loggedIn: boolean }
@@ -16,48 +22,63 @@ class Auth extends React.Component<
     user: null,
     loggedIn: false,
   };
+  unsub: firebase.Unsubscribe;
   componentDidMount() {
-    firebase.auth().onAuthStateChanged((user) => {
+    this.unsub = firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         this.setState({ user, loggedIn: true });
-        // ...
       } else {
-        // User is signed out.
-        // ...
         this.setState({ user: null, loggedIn: false });
       }
     });
   }
-  signIn() {
-    app
-      .auth()
-      .signInWithPopup(provider)
-      .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        // The signed-in user info.
-        var user = result.user;
-        // ...
+  componentWillUnmount() {
+    this.unsub();
+  }
 
-        this.setState({ user, loggedIn: true });
-      })
-      .catch(function(error) {});
+  signIn() {
+    app.auth().signInWithPopup(provider);
+  }
+  signOut() {
+    app.auth().signOut();
   }
   render() {
     return this.props.children({
       loggedIn: this.state.loggedIn,
       user: this.state.user,
+      signIn: this.signIn,
+      signOut: this.signOut,
     });
   }
 }
 
-const Home = ({ user }: { user: firebase.User }) => (
-  <div>hello {user.displayName}</div>
+const Home = ({
+  user,
+  signOut,
+}: {
+  user: firebase.User;
+  signOut: () => void;
+}) => (
+  <div>
+    hello {user.displayName} <button onClick={signOut}>sign out</button>
+  </div>
+);
+
+const SignIn = ({ signIn }: { signIn: () => void }) => (
+  <div>
+    you are not logged in
+    <button onClick={signIn}>sign in with Google</button>
+  </div>
 );
 const Dash = () => (
   <div>
     <Auth>
-      {({ loggedIn, user }) =>
-        loggedIn ? <Home user={user} /> : <div>not hello</div>
+      {({ loggedIn, user, signIn, signOut }) =>
+        loggedIn ? (
+          <Home user={user} signOut={signOut} />
+        ) : (
+          <SignIn signIn={signIn} />
+        )
       }
     </Auth>
   </div>
